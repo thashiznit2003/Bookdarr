3bb95d4e2e4a176bc4445449f85b9ad818f6dc2c
Joe Blakely <joe@Joes-Mac-mini.local>
2026-01-17 02:18:23 -0600

    Return created user library resource via controller

--- src/Readarr.Api.V1/Books/UserLibraryController.cs
+++ src/Readarr.Api.V1/Books/UserLibraryController.cs
@@ -47,7 +47,7 @@ namespace Readarr.Api.V1.Books
             var userBook = _libraryService.AddOrGetUserBook(user.Id, resource.BookId, resource.WantsEbook, resource.WantsAudiobook);
             var mapped = MapUserBook(userBook);
 
-            return Created(mapped.Id, mapped);
+            return Created(mapped.Id);
         }
 
         [HttpDelete("{bookId:int}")]
