c1739148bb83cb23a078535ec72e7f80d6cba2c8
Joe Blakely <joe@Joes-Mac-mini.local>
2026-01-25 13:07:11 -0600

    Fix QueueService using order

--- src/NzbDrone.Core/Queue/QueueService.cs
+++ src/NzbDrone.Core/Queue/QueueService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Crypto;
 using NzbDrone.Core.Books;
 using NzbDrone.Core.Download.TrackedDownloads;
